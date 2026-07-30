ace900a6110c0f8c4ff43ec7c19d989fa31601c4
tientrysoft <tienvm886@gmail.com>
2026-07-30 22:48:02 +0700

    initialize backend stuff

---
+++ src/test/java/study_web/cus/CusApplicationTests.java
@@ -0,0 +1,13 @@
+package study_web.cus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
