514659657fc15cc60bdb88a9b6e0a9d7c0c59926
LeonLH928 <lehuynhanhtuan123@gmail.com>
2026-08-21 13:23:51 +0700

    fix(exception,error-code): remove unban exception and add admin exception + error codes

---
+++ src/main/java/studyweb/cus/exception/admin/AdminErrorCode.java
@@ -0,0 +1,23 @@
+package studyweb.cus.exception.admin;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
+import studyweb.cus.exception.BaseErrorCode;
+
+@Getter
+@Accessors(fluent = true)
+@RequiredArgsConstructor
+public enum AdminErrorCode implements BaseErrorCode {
+  USER_BANNED("ADMIN_001", "User is permanently banned", HttpStatus.FORBIDDEN),
+  USER_LOCKED("ADMIN_002", "User is locked. Please unlock first", HttpStatus.FORBIDDEN),
+  USER_NOT_FOUND("ADMIN_003", "User not found", HttpStatus.NOT_FOUND),
+  ROLE_NOT_ALLOWED("ADMIN_004", "Actions not allowed for this role", HttpStatus.FORBIDDEN),
+  USER_EXISTED("ADMIN_005", "User already existed", HttpStatus.CONFLICT);
+
+  private final String code;
+  private final String message;
+  private final HttpStatus httpStatus;
+}
