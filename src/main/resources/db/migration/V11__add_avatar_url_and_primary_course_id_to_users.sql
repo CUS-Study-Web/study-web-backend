670d7cd74b928a19693118d7881c79d9a1c95b70
LeonLH928 <lehuynhanhtuan123@gmail.com>
2026-08-25 01:17:09 +0700

    fix(flyway): rename to prevent mismatch between entity and flyway

--- src/main/resources/db/migration/V11__add_avatar_url_and_primary_course_id_to_users.sql
+++ src/main/resources/db/migration/V11__add_avatar_url_and_primary_course_id_to_users.sql
@@ -1,3 +1,3 @@
 ALTER TABLE users
     ADD COLUMN avatar_url VARCHAR(500),
-    ADD COLUMN primary_course_id UUID REFERENCES courses(id) ON DELETE SET NULL;
+    ADD COLUMN course_id UUID REFERENCES courses(id) ON DELETE SET NULL;
